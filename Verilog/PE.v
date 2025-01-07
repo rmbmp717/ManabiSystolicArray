d2b7ec6046e30de74eb2e066aacd88bf442a12b1
nishiharu <179163361+rmbmp717@users.noreply.github.com>
2025-01-08 06:25:49 +0900

    Create PE.v

---
+++ Verilog/PE.v
@@ -0,0 +1,24 @@
+module PE(
+
+// Input
+input Clock
+input rst_n
+
+// Input controll signal
+input data_clear
+input en_shift_right
+input en_shift_bottom
+
+input [15:0] b_reg
+input b_we
+
+// Output
+output [15:0] a_shift_to_righit
+output [15:0] partial_sum_to_bottom
+
+TBD
+) {
+
+TBD
+
+}
