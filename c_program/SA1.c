a17cf1966a7b3cc5c5198eed75911f0e3be895b4
nishiharu <179163361+rmbmp717@users.noreply.github.com>
2025-02-04 23:42:46 +0900

    Add files via upload

--- c_program/SA1.c
+++ c_program/SA1.c
@@ -1,8 +1,16 @@
 #include <stdint.h>  // uint32_t 型を使うために必要
 
-#define TARGET_ADDR  0x150
+#define TARGET_ADDR  0x250
 #define UART_ADDR    0x4F0
 #define DMA_ADDR     0x400  
+
+#define RIGHT_SHIFT     0xFF
+#define B_DATA_SHIFT    0xFE
+#define BOTTOM_SHIFT    0xFD
+#define A_DATA_IN       0xFC
+#define B_DATA_IN       0xFB
+
+#define DATA_OUT_ADDR   0x300
 
 // 固定アドレスでメモリを確保する方法
 volatile uint32_t sum[4][4];
@@ -11,63 +19,83 @@ int _start(void)
 {
     // 書き込み元の4x4行列
     volatile uint8_t arrA[4][4] = {
-        {1, 34, 51, 68},
-        {5, 12, 19, 16},
-        {3, 17, 7, 4},
-        {21, 38, 25, 0}
+        {1, 3, 5, 8},
+        {5, 1, 1, 6},
+        {3, 1, 7, 4},
+        {1, 3, 2, 0}
     };
 
     volatile uint8_t arrB[4][4] = {
-        {1, 3, 154, 11},
-        {18, 205, 22, 39},
-        {20, 25, 25, 55},
-        {15, 25, 25, 17}
+        {1, 3, 4, 1},
+        {8, 5, 2, 3},
+        {2, 2, 5, 5},
+        {5, 5, 5, 7}
     };
 
-    // sumの内容を初期化または計算
-    /*
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            //sum[i][j] = i * 4 + j; // デモ用の値
-            sum[i][j] = arrA[i][j] + arrB[i][j];
-        }
-    }
-
-    // メモリに書き込む
     volatile uint32_t *p = (volatile uint32_t *)TARGET_ADDR;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            *p++ = sum[i][j];
-        }
-    }
-    */
-    volatile uint32_t *p = (volatile uint32_t *)TARGET_ADDR;
-
-    // 終了マークをメモリに書き込む
     volatile uint32_t *p2 = (volatile uint32_t *)(uintptr_t)UART_ADDR;
-    *p2 = 0x02;
 
-    *p2 = 0x04;
+    // 右シフト テスト
+    *p2 = RIGHT_SHIFT;
 
-    *p2 = 0x06;
-    *p2 = 0x01;
+    // 下シフト テスト
+    *p2 = B_DATA_SHIFT;
+
+    // A Matrix データ転送
+    *p2 = A_DATA_IN;
     *p = 0x00;
 
-    *p2 = 0x02;
+    *p2 = 0x01;     // 1
+    *p = 0x00;
+    *p2 = 0x01;     // 1
+    *p = 0x00;
+    *p2 = 0x01;     // 1
+    *p = 0x00;
+    *p2 = 0x01;     // 1
     *p = 0x00;
 
-    *p2 = 0x03;
+    // B Matrix データ転送
+    *p2 = B_DATA_IN;
     *p = 0x00;
 
-    *p2 = 0x04;
+    *p2 = 0x01;     // 1
+    *p = 0x00;
+    *p2 = 0x01;     // 1
+    *p = 0x00;
+    *p2 = 0x01;     // 1
+    *p = 0x00;
+    *p2 = 0x01;     // 1
     *p = 0x00;
 
-    // 指定アドレスのデータを読み取る
+    // 右シフト x 4
+    for (int i = 0; i < 4; i++) {
+        *p2 = RIGHT_SHIFT;
+        *p = 0x00;
+    }
+
+    // 下データシフト x 4
+    for (int i = 0; i < 4; i++) {
+        *p2 = B_DATA_SHIFT;
+        *p = 0x00;
+    }
+
+    // 下シフト x 4
+    for (int i = 0; i < 4; i++) {
+        *p2 = BOTTOM_SHIFT;
+        *p = 0x00;
+    }
+
+    // wait
+    for (int i = 0; i < 20; i++) {
+        *p = 0x00;
+    }
+
+    // DMAのデータを読み取る
     volatile uint32_t *read_ptr = (volatile uint32_t *)DMA_ADDR;
     uint32_t read_data = *read_ptr;
 
-    // 読み取ったデータをUARTに送信
-    volatile uint32_t *p3 = (volatile uint32_t *)(uintptr_t) 0x280;
+    // 読み取ったデータをDATA_OUT_ADDRに書き込む
+    volatile uint32_t *p3 = (volatile uint32_t *)(uintptr_t) DATA_OUT_ADDR;
     *p3 = read_data;
 
     // 無限ループ
